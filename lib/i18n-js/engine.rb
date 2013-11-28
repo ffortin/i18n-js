325bf4aeffd9b7822469c071d42656a92108c2db
Francois Fortin <francois.fortin@adgear.com>
2013-11-28 15:16:54 -0500

    Attempts to remove all cache processing

--- lib/i18n-js/engine.rb
+++ lib/i18n-js/engine.rb
@@ -7,60 +7,15 @@ module SimplesIdeias
                                                 :before => "i18n-js.initialize" do
         next unless SimplesIdeias::I18n.has_asset_pipeline?
 
-        config = I18n.config_file
-        cache_file = I18n::Engine.load_path_hash_cache
 
         if Rails.version >= "3.1"
           Rails.application.assets.register_preprocessor "application/javascript", Preprocessor
-        else
-          Rails.application.assets.register_preprocessor "application/javascript", :"i18n-js_dependencies" do |context, data|
-            if context.logical_path == I18N_TRANSLATIONS_ASSET
-              context.depend_on(config) if I18n.config?
-              # also set up dependencies on every locale file
-              ::I18n.load_path.each {|path| context.depend_on(path)}
-
-              # Set up a dependency on the contents of the load path
-              # itself. In some situations it is possible to get here
-              # before the path hash cache file has been written; in
-              # this situation, write it now.
-              I18n::Engine.write_hash! unless File.exists?(cache_file)
-              context.depend_on(cache_file)
-            end
-
-            data
-          end
         end
       end
 
       # rewrite path cache hash at startup and before each request in development
       config.to_prepare do
-        next unless SimplesIdeias::I18n.has_asset_pipeline?
-        SimplesIdeias::I18n::Engine.write_hash_if_changed unless Rails.env.production?
-      end
-
-      def self.load_path_hash_cache
-        @load_path_hash_cache ||= Rails.root.join("tmp/i18n-js.cache")
-      end
-
-      def self.write_hash_if_changed
-        load_path_hash = ::I18n.load_path.hash
-
-        if load_path_hash != cached_load_path_hash
-          self.cached_load_path_hash = load_path_hash
-          write_hash!
-        end
-      end
-
-      def self.write_hash!
-        FileUtils.mkdir_p Rails.root.join("tmp")
-
-        File.open(load_path_hash_cache, "w+") do |f|
-          f.write(cached_load_path_hash)
-        end
-      end
-
-      class << self
-        attr_accessor :cached_load_path_hash
+        next
       end
     end
   end
--- lib/i18n-js/middleware.rb
+++ lib/i18n-js/middleware.rb
@@ -6,25 +6,11 @@ module SimplesIdeias
       end
 
       def call(env)
-        @cache = nil
         verify_locale_files!
         @app.call(env)
       end
 
       private
-      def cache_path
-        @cache_path ||= Rails.root.join("tmp/cache/i18n-js.yml")
-      end
-
-      def cache
-        @cache ||= begin
-          if cache_path.exist?
-            YAML.load_file(cache_path) || {}
-          else
-            {}
-          end
-        end
-      end
 
       # Check if translations should be regenerated.
       # ONLY REGENERATE when these conditions are met:
@@ -34,25 +20,7 @@ module SimplesIdeias
       # # Translation file has been updated
       #
       def verify_locale_files!
-        valid_cache = []
-        new_cache = {}
-
-        valid_cache.push cache_path.exist?
-        valid_cache.push ::I18n.load_path.uniq.size == cache.size
-
-        ::I18n.load_path.each do |path|
-          changed_at = File.mtime(path).to_i
-          valid_cache.push changed_at == cache[path]
-          new_cache[path] = changed_at
-        end
-
-        unless valid_cache.all?
-          File.open(cache_path, "w+") do |file|
-            file << new_cache.to_yaml
-          end
-
-          SimplesIdeias::I18n.export!
-        end
+        SimplesIdeias::I18n.export!
       end
     end
   end
